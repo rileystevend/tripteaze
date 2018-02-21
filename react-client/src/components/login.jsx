f7e3b35959a1682d96891c46bb06cdc1caadb607
Trent <trenthendricks2@gmail.com>
2018-02-21 14:58:57 -0600

    should be fine, untested...

--- react-client/src/components/login.jsx
+++ react-client/src/components/login.jsx
@@ -1,18 +1,13 @@
 const express = require('express');
-const Users = require('Fill_Me_In'); // need to create /////////
+const Users = require('../database-mongo/index.js'); // need to create /////////
 const bcrypt = require('bcrypt-nodejs');
-const app = expres();
+const app = express();
 
-// helper func to check if pw matches user also decrypts.
 app.checkPassword = (users, userName, pw) => {
   let match = false;
-  // this may need to be changed based off how we setup things.
   Users.forEach( user => {
-    // compares pw and each individual user's pw (returns true/false)
     let unhashedPw = bcrypt.compareSync(pw, user.attributes.password)
-    // check if the usernames match and the pw matches
     if (user.attributes.username === userName && unhashedPw) {
-      // if it does returns true resulting in a matched pw.
       match = true;
     }
   })
@@ -20,27 +15,17 @@ app.checkPassword = (users, userName, pw) => {
 }
 
 
-// Receives a login post reqest
 app.post('/login', (req, res) =>{
-  // first sets username and pw to be used to check if they are a user.
   let userName = req.body.username
   let password = req.body.password
 
-  // fetch users to then check pw against them
   Users.fetch().then((users) => {
-    // checking if returns true (means its a matched username and pw)
     if (app.checkPassword(users, userName, password)) {
-      // sets the curret session to being loggedIn
-          // this can be changed how we handle it, this just worked for me -
-          //    - before
       req.session.loggedIn = true;
-      // then redirects to new page (i have it going to defalt - will likely -
-        //  - change it to route to trip planning or userpage)
-      res.redirect(301, '/')
+      res.end();
     } else {
-      // if unmatched username/pw alerts them and redirects to login again.
-      alert('unmatching username and password')
-      res.redirect(301, '/login')
+      alert('Unmatching username and password');
+      res.end();
     }
   })
 })
@@ -54,12 +39,3 @@ app.get('/logout', (req, res) => {
    })
    res.redirect(301, /* figured could redirect to*/ '/login' /*or'/homepage' */);
 })
-
-// Possibly un-needed
-/*
-app.get('/login', (req, res) =>{
-  res.render() {
-
-  }
-})
-*/
